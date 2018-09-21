467ad03fcfe2624969f5ac3ee75f8ae2f0482808
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-09-21 17:14:24 +0200

    enhancement: move cors to method

--- server/main.ts
+++ server/main.ts
@@ -4,14 +4,12 @@ import { ApplicationModule } from './app.module';
 
 enableProdMode();
 
-const CORS_OPTIONS = {
-  methods: 'GET',
-  maxAge: 3600
-};
-
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
-  app.enableCors(CORS_OPTIONS);
+  app.enableCors({
+    methods: 'GET',
+    maxAge: 3600
+  });
   await app.listen(4000);
 }
 bootstrap().catch(err => console.error(err));
